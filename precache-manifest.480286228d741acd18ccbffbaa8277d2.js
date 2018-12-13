a21f9d3d530877c14eb2c60bdca44d945b56d53f
Jean <jeangarcialunardi95@gmail.com>
2018-12-13 11:37:07 -0400

    Updates

---
+++ precache-manifest.480286228d741acd18ccbffbaa8277d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "1a7ffd81784cae1aa2b3",
+    "url": "/portfolio/static/js/main.1a7ffd81.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/portfolio/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "1a7ffd81784cae1aa2b3",
+    "url": "/portfolio/static/css/main.e4f90ae4.chunk.css"
+  },
+  {
+    "revision": "9e55c8ca7d368b349c7af394b5028807",
+    "url": "/portfolio/index.html"
+  }
+];
