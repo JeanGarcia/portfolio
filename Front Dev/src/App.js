e4a38a4bcb6d78042be4d0790dc2d20ba7f996fc
Jean <mayerucreations@gmail.com>
2020-08-09 22:30:12 +0200

    new structure, back and front

---
+++ Front Dev/src/App.js
@@ -0,0 +1,20 @@
+
+// MAIN PAGE, defining the basic skeleton of the page. 
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    // TODO: side navigation menu, main boxe on left.  
+    return (
+      
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
